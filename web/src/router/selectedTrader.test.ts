johnwick2921-cyber/nofx

fix(web): trader selection persists — no more snap-back to last-created
The dashboard defaulted to traders[0], which (traders API is creation-DESC)
is the LAST-CREATED trader, and selection lived only in the URL (?trader=)
plus in-memory state — so any load without the param (header nav to a bare
/dashboard, new tab, bookmark, F5 after the param was dropped) snapped the
view back to the newest trader.

Make selection first-class (router/selectedTrader.ts):
- resolveSelectedTrader — one pure authority: URL → in-memory → localStorage
  → a STABLE-sorted first trader (name then id; never creation-order [0]).
- localStorage load/save/clear so a View click survives new tabs / refresh /
  bookmarks; a persisted id that no longer resolves (deleted trader) reports
  clearStored → the caller drops the stale entry and falls back gracefully.
- AppRoutes DashboardRoute now resolves via this and persists every
  resolution to BOTH the URL and localStorage; idempotent on a stable
  selection, so a poll/remount never silently switches traders.

FE-only; additive; no kernel/trader/risk logic touched. Tests: priority
order (url>memory>storage>fallback), deleted-id fallback + clear, stale-URL
fall-through, stable deterministic sort. 17/17 vitest green; tsc+build green.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

web/src/router/selectedTrader.test.ts
@@ -1,0 +1,103 @@
+import { describe, it, expect } from 'vitest'
+import type { TraderInfo } from '../types'
+import { resolveSelectedTrader, byStableTraderOrder } from './selectedTrader'
+
+const mk = (id: string, name: string): TraderInfo => ({
+  trader_id: id,
+  trader_name: name,
+  ai_model: 'deepseek-v4-pro',
+  is_running: true,
+})
+
+// Two traders sharing the real user id-prefix. In the traders API they arrive
+// creation-DESC, so index 0 is the LAST-CREATED — the old snap-back default.
+const HOANG = mk('8d5c8af5_hoang_1781246265', 'hoang')
+const FIFTEEN = mk('8d5c8af5_15m_1786221895', '15m')
+// API order: newest first → [FIFTEEN, HOANG] if 15m was created last.
+const TRADERS = [FIFTEEN, HOANG]
+
+describe('resolveSelectedTrader — priority order', () => {
+  it('URL wins over everything', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      HOANG.trader_id, // url
+      FIFTEEN.trader_id, // memory
+      FIFTEEN.trader_id // storage
+    )
+    expect(r.source).toBe('url')
+    expect(r.trader).toBe(HOANG)
+    expect(r.clearStored).toBe(false)
+  })
+
+  it('in-memory wins when no URL', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      HOANG.trader_id, // memory
+      FIFTEEN.trader_id // storage
+    )
+    expect(r.source).toBe('memory')
+    expect(r.trader).toBe(HOANG)
+  })
+
+  it('localStorage restores the selection on a fresh load (no url, no memory)', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      undefined,
+      HOANG.trader_id // storage
+    )
+    expect(r.source).toBe('storage')
+    expect(r.trader).toBe(HOANG)
+  })
+
+  it('fallback is the STABLE-sorted first, NOT creation-order traders[0]', () => {
+    // Nothing selected anywhere → must NOT snap to the last-created (FIFTEEN at
+    // index 0); stable order is by name → "15m" < "hoang", so 15m; if names were
+    // reversed the id tiebreak still makes it deterministic (never index-based).
+    const r = resolveSelectedTrader(TRADERS, undefined, undefined, undefined)
+    expect(r.source).toBe('fallback')
+    expect(r.trader).toBe([...TRADERS].sort(byStableTraderOrder)[0])
+    expect(r.clearStored).toBe(false)
+  })
+})
+
+describe('resolveSelectedTrader — deleted-id fallback', () => {
+  it('a stored id that no longer exists → fallback + clearStored', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      undefined,
+      'deleted_trader_id_gone'
+    )
+    expect(r.source).toBe('fallback')
+    expect(r.clearStored).toBe(true) // caller clears the stale localStorage entry
+    expect(r.trader).toBeDefined()
+  })
+
+  it('a stale URL slug falls through to the next source (never hijacks)', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      'no-such-slug',
+      undefined,
+      HOANG.trader_id
+    )
+    expect(r.source).toBe('storage') // storage still resolves
+    expect(r.trader).toBe(HOANG)
+  })
+
+  it('empty / missing trader list → none', () => {
+    expect(resolveSelectedTrader([], 'x', 'y', 'z').source).toBe('none')
+    expect(
+      resolveSelectedTrader(undefined, 'x', 'y', 'z').trader
+    ).toBeUndefined()
+  })
+})
+
+describe('byStableTraderOrder', () => {
+  it('is deterministic and independent of array order', () => {
+    const a = [FIFTEEN, HOANG].sort(byStableTraderOrder)
+    const b = [HOANG, FIFTEEN].sort(byStableTraderOrder)
+    expect(a.map((t) => t.trader_id)).toEqual(b.map((t) => t.trader_id))
+  })
+})

johnwick2921-cyber/vlautoagenttraderv1

web/src/router/selectedTrader.test.ts
@@ -1,0 +1,103 @@
+import { describe, it, expect } from 'vitest'
+import type { TraderInfo } from '../types'
+import { resolveSelectedTrader, byStableTraderOrder } from './selectedTrader'
+
+const mk = (id: string, name: string): TraderInfo => ({
+  trader_id: id,
+  trader_name: name,
+  ai_model: 'deepseek-v4-pro',
+  is_running: true,
+})
+
+// Two traders sharing the real user id-prefix. In the traders API they arrive
+// creation-DESC, so index 0 is the LAST-CREATED — the old snap-back default.
+const ZETA = mk('d7a1f9b3_zeta_1781246265', 'zeta')
+const FIFTEEN = mk('d7a1f9b3_15m_1786221895', '15m')
+// API order: newest first → [FIFTEEN, ZETA] if 15m was created last.
+const TRADERS = [FIFTEEN, ZETA]
+
+describe('resolveSelectedTrader — priority order', () => {
+  it('URL wins over everything', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      ZETA.trader_id, // url
+      FIFTEEN.trader_id, // memory
+      FIFTEEN.trader_id // storage
+    )
+    expect(r.source).toBe('url')
+    expect(r.trader).toBe(ZETA)
+    expect(r.clearStored).toBe(false)
+  })
+
+  it('in-memory wins when no URL', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      ZETA.trader_id, // memory
+      FIFTEEN.trader_id // storage
+    )
+    expect(r.source).toBe('memory')
+    expect(r.trader).toBe(ZETA)
+  })
+
+  it('localStorage restores the selection on a fresh load (no url, no memory)', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      undefined,
+      ZETA.trader_id // storage
+    )
+    expect(r.source).toBe('storage')
+    expect(r.trader).toBe(ZETA)
+  })
+
+  it('fallback is the STABLE-sorted first, NOT creation-order traders[0]', () => {
+    // Nothing selected anywhere → must NOT snap to the last-created (FIFTEEN at
+    // index 0); stable order is by name → "15m" < "zeta", so 15m; if names were
+    // reversed the id tiebreak still makes it deterministic (never index-based).
+    const r = resolveSelectedTrader(TRADERS, undefined, undefined, undefined)
+    expect(r.source).toBe('fallback')
+    expect(r.trader).toBe([...TRADERS].sort(byStableTraderOrder)[0])
+    expect(r.clearStored).toBe(false)
+  })
+})
+
+describe('resolveSelectedTrader — deleted-id fallback', () => {
+  it('a stored id that no longer exists → fallback + clearStored', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      undefined,
+      undefined,
+      'deleted_trader_id_gone'
+    )
+    expect(r.source).toBe('fallback')
+    expect(r.clearStored).toBe(true) // caller clears the stale localStorage entry
+    expect(r.trader).toBeDefined()
+  })
+
+  it('a stale URL slug falls through to the next source (never hijacks)', () => {
+    const r = resolveSelectedTrader(
+      TRADERS,
+      'no-such-slug',
+      undefined,
+      ZETA.trader_id
+    )
+    expect(r.source).toBe('storage') // storage still resolves
+    expect(r.trader).toBe(ZETA)
+  })
+
+  it('empty / missing trader list → none', () => {
+    expect(resolveSelectedTrader([], 'x', 'y', 'z').source).toBe('none')
+    expect(
+      resolveSelectedTrader(undefined, 'x', 'y', 'z').trader
+    ).toBeUndefined()
+  })
+})
+
+describe('byStableTraderOrder', () => {
+  it('is deterministic and independent of array order', () => {
+    const a = [FIFTEEN, ZETA].sort(byStableTraderOrder)
+    const b = [ZETA, FIFTEEN].sort(byStableTraderOrder)
+    expect(a.map((t) => t.trader_id)).toEqual(b.map((t) => t.trader_id))
+  })
+})